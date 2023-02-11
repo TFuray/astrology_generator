a7043014db353b9ea34560b92b806c0295a5b0f5
TFuray <pk.master881@gmail.com>
2023-02-11 15:11:14 -0800

    changed input from month to sign

--- js/main.js
+++ js/main.js
@@ -14,12 +14,12 @@ const starSigns = [
   'sagittarius',
   'capricorn',
   'aquarius',
-  'pices'
+  'pisces'
 ]
 
 const apiUrl = 'https://aztro.sameerkumar.website?sign='
 
-const getTodaysReadings = () => {
+const getTodaysReadingsAll = () => {
   starSigns.forEach(sign => {
     fetch(apiUrl + sign + '&day=today', {
       method: 'POST',
@@ -38,42 +38,43 @@ const getTodaysReadings = () => {
 }
 
 function getTodaysReadingsByMonth () {
-  let month = Number(document.querySelector('input').value) - 1
-
-  fetch(apiUrl + starSigns[month] + '&day=today', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-      function toggleHidden () {
-        const listP = document.querySelectorAll('p')
-        for (const element of listP) {
-          element.classList.toggle('hidden')
+  let signInput = document.querySelector('input').value.toLowerCase()
+  starSigns.includes(signInput)
+    ? fetch(apiUrl + signInput + '&day=today', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
         }
-      }
-      toggleHidden()
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          function toggleHidden () {
+            const listP = document.querySelectorAll('p')
+            for (const element of listP) {
+              element.classList.toggle('hidden')
+            }
+          }
+          toggleHidden()
 
-      const sign = document.querySelector('#sign')
-      const desc = document.querySelector('#desc')
-      const compatibility = document.querySelector('#compatability')
-      const mood = document.querySelector('#mood')
-      const color = document.querySelector('#color')
-      const luckyNumber = document.querySelector('#luckyNumber')
-      const luckyTime = document.querySelector('#luckyTime')
+          const sign = document.querySelector('#sign')
+          const desc = document.querySelector('#desc')
+          const compatibility = document.querySelector('#compatability')
+          const mood = document.querySelector('#mood')
+          const color = document.querySelector('#color')
+          const luckyNumber = document.querySelector('#luckyNumber')
+          const luckyTime = document.querySelector('#luckyTime')
 
-      sign.innerText = starSigns[month].toUpperCase()
-      desc.innerText = data.description
-      compatibility.innerText= data.compatibility
-      mood.innerText = data.mood
-      color.innerText = data.color
-      luckyNumber.innerText = data.lucky_number
-      luckyTime.innerText = data.lucky_time
-    })
-    .catch(err => {
-      console.log(`error ${err}`)
-    })
+          sign.innerText = signInput.toUpperCase()
+          desc.innerText = data.description
+          compatibility.innerText = data.compatibility
+          mood.innerText = data.mood
+          color.innerText = data.color
+          luckyNumber.innerText = data.lucky_number
+          luckyTime.innerText = data.lucky_time
+        })
+        .catch(err => {
+          console.log(`error ${err}`)
+        })
+    : document.querySelector('h3').classList.toggle('hidden')
 }
