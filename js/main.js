85efe7f5f8b23fb3f372a3e5fe652e384492bdf0
Andrew Quach <andrewrquach@gmail.com>
2023-02-13 15:55:57 -0800

    fix: fix button from toggling hidden elements every click

--- js/main.js
+++ js/main.js
@@ -37,44 +37,47 @@ const getTodaysReadingsAll = () => {
   })
 }
 
-function getTodaysReadingsByMonth () {
+function getTodaysReadingsByMonth() {
   let signInput = document.querySelector('input').value.toLowerCase()
   starSigns.includes(signInput)
     ? fetch(apiUrl + signInput + '&day=today', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-        .then(res => res.json())
-        .then(data => {
-          console.log(data)
-          function toggleHidden () {
-            const listP = document.querySelectorAll('p')
-            for (const element of listP) {
-              element.classList.toggle('hidden')
-            }
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        function toggleHidden() {
+          const listP = document.querySelectorAll('p')
+          for (const element of listP) {
+            element.classList.toggle('hidden')
           }
-          toggleHidden()
+        }
+        function isHidden() {
+          return [...document.querySelectorAll('p')].every(e => e.classList.contains('hidden'))
+        }
+        if (isHidden()) toggleHidden()
 
-          const sign = document.querySelector('#sign')
-          const desc = document.querySelector('#desc')
-          const compatibility = document.querySelector('#compatability')
-          const mood = document.querySelector('#mood')
-          const color = document.querySelector('#color')
-          const luckyNumber = document.querySelector('#luckyNumber')
-          const luckyTime = document.querySelector('#luckyTime')
+        const sign = document.querySelector('#sign')
+        const desc = document.querySelector('#desc')
+        const compatibility = document.querySelector('#compatability')
+        const mood = document.querySelector('#mood')
+        const color = document.querySelector('#color')
+        const luckyNumber = document.querySelector('#luckyNumber')
+        const luckyTime = document.querySelector('#luckyTime')
 
-          sign.innerText = signInput.toUpperCase()
-          desc.innerText = data.description
-          compatibility.innerText = data.compatibility
-          mood.innerText = data.mood
-          color.innerText = data.color
-          luckyNumber.innerText = data.lucky_number
-          luckyTime.innerText = data.lucky_time
-        })
-        .catch(err => {
-          console.log(`error ${err}`)
-        })
+        sign.innerText = signInput.toUpperCase()
+        desc.innerText = data.description
+        compatibility.innerText = data.compatibility
+        mood.innerText = data.mood
+        color.innerText = data.color
+        luckyNumber.innerText = data.lucky_number
+        luckyTime.innerText = data.lucky_time
+      })
+      .catch(err => {
+        console.log(`error ${err}`)
+      })
     : document.querySelector('h3').classList.toggle('hidden')
 }
